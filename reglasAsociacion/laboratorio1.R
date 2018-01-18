bead41138264ab746a2387924193860d0c56d6f2
merelo <merelo.miguel@gmail.com>
2018-01-18 15:51:38 +0100

    r

--- reglasAsociacion/laboratorio1.R
+++ reglasAsociacion/laboratorio1.R
@@ -26,3 +26,47 @@ summary(Epub)
 image(Epub)
 
 itemFrequencyPlot(Adult,support=0.1,cex.names=0.8)
+
+iAdult<-apriori(Adult, parameter = list(support = 0.1, target="frequent"))
+iAdult<-sort(iAdult, by="support") # Los ordenamosporel valor del soporte
+inspect(head(iAdult, n=10)) # Inspeccionamoslos 10 primeros
+
+
+size(iAdult)
+barplot(table(size(iAdult)), xlab="itemsetsize", ylab="count")
+inspect(iAdult[size(iAdult)==1])
+
+#maximales
+imaxAdult<-iAdult[is.maximal(iAdult)]
+inspect(head(sort(imaxAdult, by="support")))
+
+#itemsets cerrados
+icloAdult<-iAdult[is.closed(iAdult)]
+inspect(head(sort(icloAdult, by="support")))
+
+#grafica itemset frecuentes, cerrados y maximales
+barplot( c(frequent=length(iAdult), closed=length(icloAdult), maximal=length(imaxAdult)), ylab="count", xlab="itemsets")
+
+#minimo soporte 0.1 confianza 0.8
+rules <-apriori(Adult, parameter = list(support = 0.1, confidence = 0.8, minlen= 2))
+summary(rules)
+
+#vemos reglas
+inspect(head(rules))
+quality(head(rules)) #medidas de calidad
+
+#ordenar por valor de confianza
+rulesSorted= sort(rules, by = "confidence")
+inspect(head(rulesSorted))
+
+#subconjunto que cumpla una regla
+rulesRaceWhite<-subset(rules, subset = lhs %in% "race=White" & lift > 1.2)
+inspect(head(rulesRaceWhite))
+
+#eliminar reglas redundantes
+subsetMatrix<-is.subset(rulesSorted, rulesSorted)
+subsetMatrix[lower.tri(subsetMatrix, diag=TRUE)] <-FALSE
+redundant <-colSums(subsetMatrix, na.rm=TRUE) >= 1
+rulesPruned<-rulesSorted[!redundant] # remove redundant rules
+inspect(head(rulesPruned))
+
